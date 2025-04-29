026d033dadad8e6fff4f9e4142bc5a81ad1d5041
Alessandro Ceruti <alessando@fido.id>
2025-04-29 14:04:02 +0200

    Created teamssection

--- src/sections/TeamsSection/index.tsx
+++ src/sections/TeamsSection/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { SiteSection } from "../../App";
+import { styled } from "@mui/material";
 
 const TeamsSection: React.FC<{
   setActiveSection: React.Dispatch<React.SetStateAction<SiteSection>>;
 }> = ({ setActiveSection }) => {
-  return <div>TeamsSection</div>;
+  return <TeamsSectionWrapper>TeamsSection</TeamsSectionWrapper>;
 };
 
 export default TeamsSection;
+
+const TeamsSectionWrapper = styled("div")({
+  width: `100vw`,
+  height: `100vh`,
+  display: `flex`,
+  justifyContent: `center`,
+  alignItems: `center`,
+});
